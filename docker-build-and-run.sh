f2a758d4402bf9e8c6a19abf5665e46a70e96bb6
kiki-kanri <a470666@gmail.com>
2024-10-04 22:37:18 +0800

    chore: update compose file and script

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd "$(dirname "$(readlink -f "$0")")"
+
 if [ "$1" = '-p' ]; then
 	docker compose pull
 fi
 
-docker compose up -d --remove-orphans
+docker compose build && docker compose up -d --remove-orphans
