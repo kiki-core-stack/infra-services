99c75dcdc8a17e6034814d2cb623c8e808588d2e
kiki-kanri <a470666@gmail.com>
2024-11-11 00:08:43 +0800

    feat: add ld preload compilation command to build-and-run script

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -6,4 +6,5 @@ if [ "$1" = '-p' ]; then
 	docker compose pull
 fi
 
-docker compose build && docker compose up -d --remove-orphans
+gcc -march=native -fPIC -O3 -shared -Werror=format-security -Wformat -o ./libforce_enable_thp.so ./force_enable_thp.c -ldl &&
+	docker compose build && docker compose up -d --remove-orphans
