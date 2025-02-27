8a65e4514bbbf2a907cbb1a1914492f87ee100d6
kiki-kanri <a470666@gmail.com>
2025-02-27 18:22:10 +0800

    feat: add functionality to set keydb password via env variable

---
+++ keydb/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+KEYDB_PASSWORD=$(cat /run/secrets/KEYDB_PASSWORD)
+if [ -n "$KEYDB_PASSWORD" ] && [ "$KEYDB_PASSWORD" -ne 0 ]; then
+    if [ "$KEYDB_PASSWORD" -ge 96 ]; then
+        exec keydb-server /etc/keydb/keydb.conf --requirepass "$KEYDB_PASSWORD"
+    else
+        echo 'Error: "KEYDB_PASSWORD" length must be at least 96 characters.' >&2
+        exit 1
+    fi
+else
+    exec keydb-server /etc/keydb/keydb.conf
+fi
