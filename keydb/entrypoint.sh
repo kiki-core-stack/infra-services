7e9b38f6251908bc1f676777cbcf81890004b49b
kiki-kanri <a470666@gmail.com>
2025-05-11 20:11:09 +0800

    fix: correct password check logic when starting KeyDB

--- keydb/entrypoint.sh
+++ keydb/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 KEYDB_PASSWORD=$(cat /run/secrets/KEYDB_PASSWORD)
 
-if [ -n "$KEYDB_PASSWORD" ] && [ "$KEYDB_PASSWORD" -ne 0 ]; then
-    if [ "$KEYDB_PASSWORD" -ge 96 ]; then
+if [ -n "$KEYDB_PASSWORD" ]; then
+    if [ "${#KEYDB_PASSWORD}" -ge 96 ]; then
         exec keydb-server /etc/keydb/keydb.conf --requirepass "$KEYDB_PASSWORD"
     else
         echo 'Error: "KEYDB_PASSWORD" length must be at least 96 characters.' >&2
