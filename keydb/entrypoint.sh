c14c69aba7b5dc6ed7e41e634ea05d2362dadb9c
kiki-kanri <a470666@gmail.com>
2025-06-08 09:31:47 +0800

    chore: wrap all variable expansions with `${}`

--- keydb/entrypoint.sh
+++ keydb/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 KEYDB_PASSWORD=$(cat /run/secrets/KEYDB_PASSWORD)
 
-if [ -n "$KEYDB_PASSWORD" ]; then
+if [ -n "${KEYDB_PASSWORD}" ]; then
     if [ "${#KEYDB_PASSWORD}" -ge 96 ]; then
-        exec keydb-server /etc/keydb/keydb.conf --requirepass "$KEYDB_PASSWORD"
+        exec keydb-server /etc/keydb/keydb.conf --requirepass "${KEYDB_PASSWORD}"
     else
         echo 'Error: "KEYDB_PASSWORD" length must be at least 96 characters.' >&2
         exit 1
