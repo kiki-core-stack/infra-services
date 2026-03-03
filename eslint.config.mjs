24a2b78b5e560f0bc43f61de1af245d4340dcf43
kiki-kanri <a470666@gmail.com>
2026-03-03 12:06:23 +0800

    chore: use eslint format docker compose files

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import { antfu } from '@antfu/eslint-config';
+import { createBaseConfigs } from '@kikiutils/eslint-config/base';
+import { createVsCodeJsonConfigs } from '@kikiutils/eslint-config/json';
+
+export default antfu({ typescript: true }, createBaseConfigs(), createVsCodeJsonConfigs());
