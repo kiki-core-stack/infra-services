d86f08564a633f1bc83ee4e909899fdf213572aa
kiki-kanri <a470666@gmail.com>
2026-08-24 18:01:11 +0800

    fix: resolve permissions errors

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
-chmod 755 ./dragonfly ./mongodb
-find ./dragonfly ./mongodb -type f -name '*.sh' -exec chmod 755 {} +
+find ./stacks -type d -exec chmod 755 {} +
+find ./stacks -type f -name '*.sh' -exec chmod 755 {} +
 
 for arg in "$@"; do
     if [[ "${arg}" == '-p' ]]; then
